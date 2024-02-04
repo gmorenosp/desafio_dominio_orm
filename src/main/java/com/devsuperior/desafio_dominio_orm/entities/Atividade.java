84fe1fb220b98a2c5c2bba45e357844dca4eee82
Gustavo Moreno <gmorenosmail@gmail.com>
2024-02-04 09:10:27 -0300

    Criacao Tabela Bloco

--- src/main/java/com/devsuperior/desafio_dominio_orm/entities/Atividade.java
+++ src/main/java/com/devsuperior/desafio_dominio_orm/entities/Atividade.java
@@ -2,6 +2,9 @@ package com.devsuperior.desafio_dominio_orm.entities;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "tb_atividade")
 public class Atividade {
@@ -19,8 +22,61 @@ public class Atividade {
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
+    @OneToMany(mappedBy = "atividade")
+    private List<Bloco> blocos = new ArrayList<>();
+
     public Atividade() {
     }
 
+    public Atividade(Integer id, String nome, String descricao, Double preco, Categoria categoria ) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.preco = preco;
+        this.categoria = categoria;
+        }
 
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public List<Bloco> getBlocos() {
+        return blocos;
+    }
 }
